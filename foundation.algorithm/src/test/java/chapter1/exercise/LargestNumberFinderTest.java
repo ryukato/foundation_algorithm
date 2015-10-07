0e32b418cd59a22e498200f99be6018a00a0b9eb
yoonyoul <yoonyoul@gmail.com>
2015-10-07 23:32:53 +0900

    Add unit test for  - default sorter  - merge sorter

--- foundation.algorithm/src/test/java/chapter1/exercise/LargestNumberFinderTest.java
+++ foundation.algorithm/src/test/java/chapter1/exercise/LargestNumberFinderTest.java
@@ -11,19 +11,44 @@ public class LargestNumberFinderTest {
 	private LagestNumberFinder findLagestNumber;
 	
 	@Before
-	public void setUp(){
-		findLagestNumber = new LagestNumberFinder(new MergeSorter<Integer>());
-	}
+	public void setUp(){}
 	
 	@Test
-	public void test(){
+	public void test_with_default_sorter(){
+		findLagestNumber = new LagestNumberFinder();
 		final Integer expectedLargestNumber = 8;
 		Integer largestNumber = (Integer) findLagestNumber.find(new Integer[]{1,2,3,4,5,6,8,7});
 		assertEquals(expectedLargestNumber, largestNumber);
 	}
 	
 	@Test
-	public void test_large_input(){
+	public void test_large_input_with_default_sorter(){
+		findLagestNumber = new LagestNumberFinder();
+		final int inputSize = 1000000;
+		Integer[] input = new Integer[inputSize];
+		input[0] = new Integer(inputSize);
+		Random random = new Random();
+		for(int i = 1; i < inputSize; i++ ){
+			input[i] = new Integer(random.nextInt(inputSize - 1));
+		}
+		
+		long startTime = System.currentTimeMillis();
+		Integer largestNumber = (Integer) findLagestNumber.find(input);
+		System.out.println("time to find : " + (System.currentTimeMillis() - startTime) / 1000);
+		assertEquals(input[0], largestNumber);
+	}
+	
+	@Test
+	public void test_with_merge_sorter(){
+		findLagestNumber = new LagestNumberFinder(new MergeSorter<Integer>());
+		final Integer expectedLargestNumber = 8;
+		Integer largestNumber = (Integer) findLagestNumber.find(new Integer[]{1,2,3,4,5,6,8,7});
+		assertEquals(expectedLargestNumber, largestNumber);
+	}
+	
+	@Test
+	public void test_large_input_with_merge_sorter(){
+		findLagestNumber = new LagestNumberFinder(new MergeSorter<Integer>());
 		final int inputSize = 1000000;
 		Integer[] input = new Integer[inputSize];
 		input[0] = new Integer(inputSize);
--- foundation.algorithm/src/test/java/chapter1/exercise/SmallestNumberFinderTest.java
+++ foundation.algorithm/src/test/java/chapter1/exercise/SmallestNumberFinderTest.java
@@ -11,19 +11,44 @@ public class SmallestNumberFinderTest {
 	private SmallestNumberFinder smallestNumberFinder;
 	
 	@Before
-	public void setUp(){
-		smallestNumberFinder = new SmallestNumberFinder(new MergeSorter<Integer>());
-	}
+	public void setUp(){}
 	
 	@Test
-	public void test(){
+	public void test_with_default_soter(){
+		smallestNumberFinder = new SmallestNumberFinder();
 		final Integer expectedSmallestNumber = 1;
 		Integer smallestNumber = (Integer) smallestNumberFinder.find(new Integer[]{1,2,3,4,5,6,8,7});
 		assertEquals(expectedSmallestNumber, smallestNumber);
 	}
 	
 	@Test
-	public void test_large_input(){
+	public void test_large_input_with_default_soter(){
+		smallestNumberFinder = new SmallestNumberFinder();
+		final int inputSize = 1000000;
+		Integer[] input = new Integer[inputSize];
+		input[0] = new Integer(0);
+		Random random = new Random();
+		for(int i = 1; i < inputSize; i++ ){
+			input[i] = new Integer(random.nextInt(inputSize - 1));
+		}
+		
+		long startTime = System.currentTimeMillis();
+		Integer smallestNumber = (Integer) smallestNumberFinder.find(input);
+		System.out.println("time to find : " + (System.currentTimeMillis() - startTime) / 1000);
+		assertEquals(input[0], smallestNumber);
+	}
+	
+	@Test
+	public void test_with_merge_soter(){
+		smallestNumberFinder = new SmallestNumberFinder(new MergeSorter<Integer>());
+		final Integer expectedSmallestNumber = 1;
+		Integer smallestNumber = (Integer) smallestNumberFinder.find(new Integer[]{1,2,3,4,5,6,8,7});
+		assertEquals(expectedSmallestNumber, smallestNumber);
+	}
+	
+	@Test
+	public void test_large_input_with_merge_soter(){
+		smallestNumberFinder = new SmallestNumberFinder(new MergeSorter<Integer>());
 		final int inputSize = 1000000;
 		Integer[] input = new Integer[inputSize];
 		input[0] = new Integer(0);
